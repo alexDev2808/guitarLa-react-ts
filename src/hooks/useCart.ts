ba1f410f3dbd56eb385a35fba1d4a97149be54ff
alexhDev2808 <colores.2808.8000@gmail.com>
2024-04-06 16:03:49 -0600

    Utility Types

--- src/hooks/useCart.ts
+++ src/hooks/useCart.ts
@@ -1,10 +1,11 @@
 import { useEffect, useState, useMemo } from "react";
+import type { CartItem } from "../types";
 
 import { db } from '../data/db';
 
 
 export const useCart = () => {
-    const initialCart = () => {
+    const initialCart  = () : CartItem[] => {
         const localStorageCart = localStorage.getItem('cart')
         return localStorageCart ? JSON.parse(localStorageCart) : []
       }
